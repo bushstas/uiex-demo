5d0731519a2f8afff9a4e35dd2e44f24fb66e786
bushstas <bushstas@mail.ru>
2018-06-26 17:51:55 +0300

    App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TabsDemo from './components/Tabs';
 import ButtonsDemo from './components/Buttons';
 import BoxDemo from './components/Box';
 import SearchFormDemo from './components/SearchForm';
-import {Section} from 'uiex';
+import {Section, SidePanel} from 'uiex';
 
 import './style.scss';
 
@@ -12,7 +12,8 @@ export default class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			page: 'SearchForm'
+			page: 'SearchForm',
+			sidePanelOpen: false
 		}
 	}
 
@@ -23,11 +24,23 @@ export default class App extends React.Component {
 					onChange={this.handleChangeMenu}
 					active={this.state.page}
 				/>
+				<SidePanel 
+					isOpen={this.state.sidePanelOpen} 
+					animation="fade-roll"
+					side="bottom"
+				/>
 				<div className="main-content">
 					{this.renderContent()}
 				</div>	
+				<button onClick={this.handle} style={{marginLeft: '400px'}}>
+					Click me
+				</button>
 			</div>
 		)
+	}
+
+	handle = () => {
+		this.setState({sidePanelOpen: !this.state.sidePanelOpen})
 	}
 
 	renderContent() {

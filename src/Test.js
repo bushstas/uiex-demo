b2a87a62cbea593683fd6286c0cd6de4bca9c686
Bushmakin Stas <bushstas@mail.ru>
2019-09-01 09:28:24 +0300

    form test

---
+++ src/Test.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+import {Form, isChanged, getChangedFields, reset} from 'uiex/Form';
+import {FormControl} from 'uiex/FormControl';
+import {FormControlGroup} from 'uiex/FormControlGroup';
+import {Checkbox} from 'uiex/Checkbox';
+import {CheckboxGroup} from 'uiex/CheckboxGroup';
+import {Input} from 'uiex/Input';
+import {SelectObject} from 'uiex/SelectObject';
+import {Button} from 'uiex/Button';
+
+const obj = {name: 'Stas', age: 22};
+const arr = [1, 2, 3];
+
+const OBJECT_OPTIONS = [
+	1, 'Hello', true, obj, arr
+];
+
+export default class Test extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: {
+				check: true,
+				name: 'Stas',
+				options: arr
+			}
+		};
+	}
+
+	handleChange = (data) => {
+		console.log(data)
+		this.setState({data});
+	}
+
+	handleClick = () => {
+		alert(isChanged('aaa'))
+		alert(getChangedFields('aaa'))
+	}
+
+	handleResetClick = () => {
+		reset('aaa')
+	}
+
+	render() {
+		return (
+			<div style={{padding: 100}}>
+				<Form
+					name="aaa"
+					data={this.state.data}
+					onChange={this.handleChange}
+					columns={4}
+				>
+					<FormControlGroup cellSize={2}>
+						<FormControl fullWidth>
+							<Checkbox name="check" label="Check">
+								<CheckboxGroup noBorder>
+									<Checkbox name="check1">
+										1
+									</Checkbox>
+									<Checkbox name="check2">
+										2
+									</Checkbox>
+								</CheckboxGroup>
+							</Checkbox>
+						</FormControl>
+						<FormControl>
+							<Input name="name" />
+						</FormControl>
+						<FormControl>
+							<SelectObject
+								name="options"
+								options={OBJECT_OPTIONS}
+							/>
+						</FormControl>
+					</FormControlGroup>
+					<Button onClick={this.handleClick}>
+						Changed?
+					</Button>
+					<Button onClick={this.handleResetClick}>
+						Reset
+					</Button>
+				</Form>
+			</div>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
+import Test from './Test';
 import AppDemo from './AppDemo';
 import {Loader} from 'uiex/Loader';
 
@@ -10,7 +11,8 @@ import {addThemes, setDefaultStyles, setDefaultProps} from 'uiex';
 // setDefaultStyles(Loader, {mask: 'border: 10px solid #000'});
 const loc = window.location.toString();
 if (/\/appdemo\.html$/.test(loc.split('#')[0])) {
-    render(<AppDemo/>, document.getElementById('root'));
+    render(<AppDemo />, document.getElementById('root'));
 } else {
-    render(<App/>, document.getElementById('root'));
+    // render(<App />, document.getElementById('root'));
+    render(<Test />, document.getElementById('root'));
 }
